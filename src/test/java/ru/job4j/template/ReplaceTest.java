911f246da68f15fc41c38d64fe5743c229713da4
bannovdaniil <baddan@mail.ru>
2022-02-07 19:05:50 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/ReplaceTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.template;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ReplaceTest {
+    @Test
+    public void whenTwoChange() {
+        Generator generator = new Replace();
+        String result = generator.produce("I am a ${name}, Who are ${subject}? ",
+                Map.of("name", "Petr Arsentev",
+                        "subject", "you"));
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenOneWordChange() {
+        Generator generator = new Replace();
+        String result = generator.produce("I am a ${name}, Who are ${subject}? ",
+                Map.of("name", "Petr Arsentev"));
+        String expected = "I am a Petr Arsentev, Who are ? ";
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenThreeWordChange() {
+        Generator generator = new Replace();
+        String result = generator.produce("I am a ${name}, Who are ${subject}? ",
+                Map.of("name", "Petr Arsentev",
+                        "subject", "you",
+                        "answer", "I"));
+        String expected = "I am a Petr Arsentev, Who are you? ";
+        assertThat(result, is(expected));
+    }
+}
